83156092d54d18458613c92ff230ab1f474af968
Alex Yoo <zerocustum89@gmail.com>
2026-02-21 20:41:29 -0800

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://AlexanderYoo.github.io',
-  base: '/AlexanderYoo.github.io',
+  site: 'https://alexanderyoo.github.io',
+  base: '/alexanderyoo.github.io',
   integrations: [icon()],
   vite: {
     plugins: [tailwindcss()]
--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -1,10 +1,10 @@
 export const NAVIGATION_DATA = {
     navigationMenu: [
-        { href: "/AlexanderYoo.github.io/home", label: "Home" },
-        { href: "/AlexanderYoo.github.io/about", label: "About" },
-        { href: "/AlexanderYoo.github.io/experience", label: "Experience" },
-        { href: "/AlexanderYoo.github.io/projects", label: "Projects" },
-        { href: "/AlexanderYoo.github.io/for-business", label: "For Business" },
-        { href: "/AlexanderYoo.github.io/contact", label: "Contact" },
+        { href: "/alexanderyoo.github.io/home", label: "Home" },
+        { href: "/alexanderyoo.github.io/about", label: "About" },
+        { href: "/alexanderyoo.github.io/experience", label: "Experience" },
+        { href: "/alexanderyoo.github.io/projects", label: "Projects" },
+        { href: "/alexanderyoo.github.io/for-business", label: "For Business" },
+        { href: "/alexanderyoo.github.io/contact", label: "Contact" },
     ]
 };
